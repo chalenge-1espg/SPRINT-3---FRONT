c54623f1efb9b4a2528ab63fddebb0f515e8f462
Lucas <lucaslugui1604@gmail.com>
2025-09-09 20:47:36 -0300

    pagina de login

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = express.Router();
+const users = [{
+    id: 1,
+    nome: 'futbolinas',
+    email: 'adm@Futbolinas.com.br',
+    senha: 'futbolinas321'
+}]
+
+routes.post('/login', (req, res) => {
+    const { email, senha } = req.body;
+    const user = users.find(user => user.email === email && user.senha === senha);
+    if (user)
+    {
+        return res.status(200).json(user);
+    }
+
+
+    return res.status(401).json({ message: 'Credenciais inválidas' })
+})
+
+
+module.exports = routes
