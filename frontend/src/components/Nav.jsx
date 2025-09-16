09d111ecd23b60a949c1cb77f1a9222d2a049933
Lucas <lucaslugui1604@gmail.com>
2025-09-16 09:07:58 -0300

    estilizando nav parte 324234234

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import Logo from '../assets/logo-futbolinas-branco.png'
 
 export default function Nav() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-gradient-to-r from-pink-600 via-purple-600 to-indigo-600 shadow-lg z-50">
-      <div className="flex justify-center items-center h-16">
-        <h1 className="text-white font-extrabold text-xl tracking-wider">FutBolinhas</h1>
+    <nav className="fixed top-0 left-0 w-full bg-gradient-to-r from-purple-800 to-purple-400 z-50">
+      <div className="max-w-6xl mx-auto px-6">
+        <div className="flex items-center h-20 space-x-3">
+          {/* Logo */}
+          <img
+            src={Logo}
+            alt="Logo FutBolinhas"
+            className="h-10 w-auto object-contain"
+          />
+
+          {/* Texto */}
+          <h1 className="text-white font-extrabold text-2xl tracking-wide">
+            FutBolinhas
+          </h1>
+        </div>
       </div>
     </nav>
   );
